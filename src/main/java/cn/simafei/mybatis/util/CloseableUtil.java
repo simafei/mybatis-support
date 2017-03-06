7d3a0adc43fd3b972d02f0bbf9fa6735381f7ab3
simafei <740284985@qq.com>
2017-03-06 20:37:01 +0800

    修复Class没有注入的bug

---
+++ src/main/java/cn/simafei/mybatis/util/CloseableUtil.java
@@ -0,0 +1,19 @@
+package cn.simafei.mybatis.util;
+
+/**
+ * 工具类
+ * Created by lirui on 2015-10-18 10:44.
+ */
+public class CloseableUtil {
+  private CloseableUtil() {
+  }
+
+  public static void closeQuietly(AutoCloseable c) {
+    if (c != null) {
+      try {
+        c.close();
+      } catch (Exception ignored) {
+      }
+    }
+  }
+}
